49b39fd74dcfa63f932eda2f1cf39f976cf0ff35
shaurya360 <84850118+shaurya360@users.noreply.github.com>
2026-06-14 01:14:48 +0530

    Fix build issues and update package dependencies

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import {
-  Wrench,
   Facebook,
-  Twitter,
   Instagram,
-  Linkedin,
   Mail,
   Phone,
   MapPin,
